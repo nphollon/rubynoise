142a20172fb7c71a1791bc653116b1abcd69979c
nphollon <nhollon@thoughtworks.com>
2013-02-16 15:30:33 -0600

    Square wave class can export to WaveFile

--- square_wave.rb
+++ square_wave.rb
@@ -7,6 +7,20 @@ class SquareWave
 	end
 
 	def export_to_wav(duration)
-		WaveFile.new
+		wav = WaveFile.new
+		num_samples = (duration * wav.sample_rate).to_i
+		samples_per_wavelength = wav.sample_rate / @frequency.to_f
+
+		(0...num_samples).each do |i|
+			wavelengths = i / samples_per_wavelength
+			phase = wavelengths - wavelengths.floor
+			if phase < 0.5
+				wav.data << 0
+			else
+				wav.data << 0xff
+			end
+		end
+
+		wav
 	end
 end
